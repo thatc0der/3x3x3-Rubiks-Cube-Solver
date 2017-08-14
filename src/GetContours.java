d33c5f006129691ca88a1e40584832b36650c7ae
Hagin <hagincubes@gmail.com>
2017-08-13 23:20:21 -0400

    finding contours for 3x3x3 is very stable. So happy. Hope it works

--- src/GetContours.java
+++ src/GetContours.java
@@ -3,12 +3,17 @@ import java.util.List;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
+import org.opencv.objdetect.*;
 public class GetContours {
 
 
@@ -19,13 +24,13 @@ static {
 	
 	public Mat captureFrame(Mat capturedFrame){
 		
-        Mat newFrame = new Mat();
+		Mat newFrame = new Mat();
         capturedFrame.copyTo(newFrame); 
         
         //Gray
         Mat gray = new Mat();
         Imgproc.cvtColor(capturedFrame, gray, Imgproc.COLOR_RGB2GRAY);
-
+        
         //Blur
         Mat blur = new Mat();
         Imgproc.blur(gray, blur, new Size(3,3));
@@ -34,7 +39,7 @@ static {
         Imgproc.Canny(blur, canny, 20, 40, 3, true);
 
         //Dilate image to increase size of lines
-        Mat kernel = Imgproc.getStructuringElement(1, new Size(3,3));
+        Mat kernel = Imgproc.getStructuringElement(2, new Size(7,7));
         Mat dilated = new Mat();
         Imgproc.dilate(canny,dilated, kernel);
 
@@ -42,45 +47,85 @@ static {
         List<MatOfPoint> contours = new ArrayList<>();
         List<MatOfPoint> squareContours = new ArrayList<>();
 
+        
+        
         //find contours
         Imgproc.findContours(dilated, contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_NONE);
 
-
+        
         //Remove contours that aren't close to a square shape.
+        //Wondering if there is a way I can improve this?
         for(int i = 0; i < contours.size(); i++){
 
-            double area = Imgproc.contourArea( contours.get(i)); 
+            double area = Imgproc.contourArea(contours.get(i)); 
             MatOfPoint2f contour2f = new MatOfPoint2f(contours.get(i).toArray());
+            
             double perimeter = Imgproc.arcLength(contour2f, true);
-
             //Found squareness equation on wiki... 
             //https://en.wikipedia.org/wiki/Shape_factor_(image_analysis_and_microscopy)
             double squareness = 4 * Math.PI * area / Math.pow(perimeter, 2);
 
             //add contour to new List if it has a square shape.
-            if(squareness >= 0.7 && squareness <= 0.9 && area >= 4000){
+            if(squareness >= 0.7 && squareness <= 0.9 && area >= 2000 && squareContours.size() < 9){
                squareContours.add(contours.get(i));
             }
         }
         
-        //Mat for getting RGB of contours
-        List<Mat> recieveRGBs = new ArrayList<>();
-        
-        //Draw square contours on saved frame in beginning of method
+        //Put overlapping code over here....
         System.out.println("square contour size: " + squareContours.size());
         for(int i = 0; i < squareContours.size(); i++){
         	
         //	squareContours.get(i).copyTo(recieveRGBs.get(i));
         	
             Imgproc.drawContours(newFrame, squareContours, i, new Scalar(255, 255, 255), 3);
-        }     
-        
-        
-        
-        
-        
-	    return newFrame;
+        }   
+	 
+    	return newFrame;
 	}
 	
+/*	private void findRGBs(Mat currFrame,List<MatOfPoint> squareContours){
+		for(int i = 0; i < squareContours.size(); i++){
+			squareContours.get(i).convertTo(currFrame, 3);
+		}
+		
+	}
 	
+	*/
+	
+	
+	
+	/*List<Point> overlappingArea = new ArrayList<>();
+    MatOfPoint2f approxCurve = new MatOfPoint2f();
+
+    for(int n = 0; n < squareContours.size(); n++){
+		
+	    //Convert contours(n) from MatOfPoint to MatOfPoint2f
+        MatOfPoint2f contour2f = new MatOfPoint2f( squareContours.get(n).toArray());
+        //Processing on mMOP2f1 which is in type MatOfPoint2f
+        double approxDistance = Imgproc.arcLength(contour2f, true)*0.02;
+        Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
+
+        //Convert back to MatOfPoint
+        MatOfPoint points = new MatOfPoint( approxCurve.toArray());
+
+        
+        // Get bounding rect of contour
+        Rect rect = Imgproc.boundingRect(points);
+        MatOfRect rects = new MatOfRect(rect);
+        MatOfInt matofint = new MatOfInt(n);
+        
+      
+        Point cooridnates = new Point(rect.x , rect.y);
+        
+        //System.out.println(rect.x + "," + rect.y);
+    	Objdetect.groupRectangles(rects, matofint, 9);
+        overlappingArea.add(cooridnates);
+        
+        for(int i = 0; i < overlappingArea.size(); i++){
+        	System.out.println("Difference: " + Math.abs(rect.x - overlappingArea.get(i).x));
+        	if (Math.abs(rect.x - overlappingArea.get(i).x) >= 5 && Math.abs(rect.y - overlappingArea.get(i).y) >= 5) {
+        		Imgproc.rectangle(newFrame, cooridnates, new Point(rect.x+rect.width,rect.y+rect.height), new Scalar(255,255,255), 3);
+        	}
+        }
+	}  */
 }
--- src/LearnContours.java
+++ src/LearnContours.java
@@ -27,7 +27,7 @@ public class LearnContours {
         //Gray
         Mat gray = new Mat();
         Imgproc.cvtColor(capturedFrame, gray, Imgproc.COLOR_BGR2GRAY);
-
+        
         //Blur
         Mat blur = new Mat();
         Imgproc.blur(gray, blur, new Size(3,3));
@@ -39,17 +39,20 @@ public class LearnContours {
         Imgcodecs.imwrite("test.png", canny);
 
         //Dilate image to increase size of lines
-        Mat kernel = Imgproc.getStructuringElement(1, new Size(3,3));
+        Mat kernel = Imgproc.getStructuringElement(2, new Size(7,7));
         Mat dilated = new Mat();
         Imgproc.dilate(canny,dilated, kernel);
 
+        Imshow m8 = new Imshow("");
+        m8.show(dilated);
 
         List<MatOfPoint> contours = new ArrayList<>();
         List<MatOfPoint> squareContours = new ArrayList<>();
-
+        Mat hierarchy = new Mat();
         //find contours
-        Imgproc.findContours(dilated, contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_NONE);
-
+        Imgproc.findContours(dilated, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_NONE);
+        
+        
         //convert image 
         Imgproc.cvtColor(capturedFrame, capturedFrame, Imgproc.COLOR_BGR2RGB);
 
@@ -72,6 +75,7 @@ public class LearnContours {
             double area = Imgproc.contourArea( contours.get(i)); 
             MatOfPoint2f contour2f = new MatOfPoint2f(contours.get(i).toArray());
             double perimeter = Imgproc.arcLength(contour2f, true);
+            
 
             //Found squareness equation on wiki... 
             // https://en.wikipedia.org/wiki/Shape_factor_(image_analysis_and_microscopy)
@@ -80,7 +84,8 @@ public class LearnContours {
            // System.out.println("Squareness: " + squareness);
 
 
-            if(squareness >= 0.7 && squareness <= 0.9 && area >= 5000){
+            if(squareness >= 0.7 && squareness <= 0.9 && area >= 2000 && squareContours.size() < 9){
+            	
                squareContours.add(contours.get(i));
             }
         }
@@ -94,33 +99,68 @@ public class LearnContours {
         
 	
 	
- 	
-	
+ 	/*
+	List<Point> overlappingArea = new ArrayList<>();
 	    	
     MatOfPoint2f approxCurve = new MatOfPoint2f();
+    int counter = 0;
 	for(int n = 0; n < squareContours.size(); n++){
 
+	
+		
 	    //Convert contours(i) from MatOfPoint to MatOfPoint2f
         MatOfPoint2f contour2f = new MatOfPoint2f( squareContours.get(n).toArray() );
-        //Processing on mMOP2f1 which is in type MatOfPoint2f
+        //Epsilon (size of rectangle)
         double approxDistance = Imgproc.arcLength(contour2f, true)*0.02;
         Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
 
         //Convert back to MatOfPoint
-        MatOfPoint points = new MatOfPoint( approxCurve.toArray() );
+        MatOfPoint points = new MatOfPoint( approxCurve.toArray());
 
         // Get bounding rect of contour
         Rect rect = Imgproc.boundingRect(points);
+        
+        Point cooridnates = new Point(rect.x , rect.y);
+        
+       // System.out.println(rect.x + "," + rect.y);
+       
+        
+        overlappingArea.add(cooridnates);
+        System.out.println("size " + overlappingArea.size());
+        for(int i = 0; i < overlappingArea.size(); i++){
+        	//System.out.println("cur: " + rect.x +"," + rect.y);
+        	//System.out.println("src: " + overlappingArea.get(i).x + "," + overlappingArea.get(i).y);
+        	if (Math.abs(rect.x - overlappingArea.get(i).x) < 5 && Math.abs(rect.y - overlappingArea.get(i).y) < 5) {
+        		counter++;
+        		Imgproc.rectangle(newFrame, cooridnates, new Point(rect.x+rect.width,rect.y+rect.height), new Scalar(255,255,255), 3);
+        	}
+        }
+        
+        overlappingArea.add(new Point(rect.x + rect.width, rect.y+rect.height));
+        
+        if(rect.x){
+        	System.out.println("INSIDE");
+        }
+        
+       
+        
+        
+     //  Point rectangle = new Point(rect.x, rect.y);
+        
+        overlappingArea.add(newCoordinatesXY);
+        if (overlappingArea.contains(Point (rect.x, rect.y)) {
+			
+		}
 
          // draw enclosing rectangle (all same color, but you could use variable i to make them unique)
-        Imgproc.rectangle(newFrame, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height),new Scalar (255, 0, 0, 255), 3); 
+        
+       // Imgproc.rectangle(newFrame, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height),new Scalar (255, 0, 0, 255), 3); 
 
-        System.out.println("Num of points: " + n);
 		}
-	
+		System.out.println(counter);*/
 	
 	showImg.show(newFrame);
-    Imgcodecs.imwrite("remove.png", newFrame);
+    Imgcodecs.imwrite("removed.png", newFrame);
 
 
 	}    	
