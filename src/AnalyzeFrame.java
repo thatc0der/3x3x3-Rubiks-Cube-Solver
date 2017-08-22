e869aacdcd9b6834cc87671b7474480f82b57105
Hagin <hagincubes@gmail.com>
2017-08-21 22:32:20 -0400

    going to try k-means clustering and see how it works. Also need to map.

--- src/AnalyzeFrame.java
+++ src/AnalyzeFrame.java
@@ -230,7 +230,8 @@ static {
 	}
 	
 
-	//indexes to track 4,13
+	//indexes to track 4,13, 22,31,40,49
+	
 	
 
 	
@@ -243,85 +244,91 @@ static {
 		
 		System.out.println(Arrays.deepToString(labArray));
 		
+		//compareColors(labArray);
+	}
+	
+	private void compareColors(double[] labArray){
+		
+		//pass color store answer in double and change values
+		
 	}
 	
 	
 	
-	
-		public double[] RGB2Lab(Color RGBColor){
-		
-		 int R = RGBColor.getRed();
-		 int G = RGBColor.getGreen();
-		 int B = RGBColor.getBlue();
-		 double r, g, b, X, Y, Z, xr, yr, zr;
+	private double[] RGB2Lab(Color RGBColor){
 
-		    // D65/2°
-		    double Xr = 95.047;  
-		    double Yr = 100.0;
-		    double Zr = 108.883;
+		int R = RGBColor.getRed();
+		int G = RGBColor.getGreen();
+		int B = RGBColor.getBlue();
+		double r, g, b, X, Y, Z, xr, yr, zr;
 
-
-		    // --------- RGB to XYZ ---------//
-
-		    r = R/255.0;
-		    g = G/255.0;
-		    b = B/255.0;
-
-		    if (r > 0.04045)
-		        r = Math.pow((r+0.055)/1.055,2.4);
-		    else
-		        r = r/12.92;
-
-		    if (g > 0.04045)
-		        g = Math.pow((g+0.055)/1.055,2.4);
-		    else
-		        g = g/12.92;
-
-		    if (b > 0.04045)
-		        b = Math.pow((b+0.055)/1.055,2.4);
-		    else
-		        b = b/12.92 ;
-
-		    r*=100;
-		    g*=100;
-		    b*=100;
-
-		    X =  0.4124*r + 0.3576*g + 0.1805*b;
-		    Y =  0.2126*r + 0.7152*g + 0.0722*b;
-		    Z =  0.0193*r + 0.1192*g + 0.9505*b;
+		// D65/2ï¿½
+		double Xr = 95.047;  
+		double Yr = 100.0;
+		double Zr = 108.883;
 
 
-		    // --------- XYZ to Lab --------- //
+		// --------- RGB to XYZ ---------//
 
-		    xr = X/Xr;
-		    yr = Y/Yr;
-		    zr = Z/Zr;
+		r = R/255.0;
+		g = G/255.0;
+		b = B/255.0;
 
-		    if ( xr > 0.008856 )
-		        xr =  (float) Math.pow(xr, 1/3.);
-		    else
-		        xr = (float) ((7.787 * xr) + 16 / 116.0);
+		if (r > 0.04045)
+			r = Math.pow((r+0.055)/1.055,2.4);
+		else
+			r = r/12.92;
 
-		    if ( yr > 0.008856 )
-		        yr =  (float) Math.pow(yr, 1/3.);
-		    else
-		        yr = (float) ((7.787 * yr) + 16 / 116.0);
+		if (g > 0.04045)
+			g = Math.pow((g+0.055)/1.055,2.4);
+		else
+			g = g/12.92;
 
-		    if ( zr > 0.008856 )
-		        zr =  (float) Math.pow(zr, 1/3.);
-		    else
-		        zr = (float) ((7.787 * zr) + 16 / 116.0);
+		if (b > 0.04045)
+			b = Math.pow((b+0.055)/1.055,2.4);
+		else
+			b = b/12.92 ;
+
+		r*=100;
+		g*=100;
+		b*=100;
+
+		X =  0.4124*r + 0.3576*g + 0.1805*b;
+		Y =  0.2126*r + 0.7152*g + 0.0722*b;
+		Z =  0.0193*r + 0.1192*g + 0.9505*b;
 
 
-		    double[] lab = new double[3];
+		// --------- XYZ to Lab --------- //
 
-		    lab[0] = (116*yr)-16;
-		    lab[1] = 500*(xr-yr); 
-		    lab[2] = 200*(yr-zr); 
+		xr = X/Xr;
+		yr = Y/Yr;
+		zr = Z/Zr;
 
-		    return lab;
-	
-	
+		if ( xr > 0.008856 )
+			xr =  (float) Math.pow(xr, 1/3.);
+		else
+			xr = (float) ((7.787 * xr) + 16 / 116.0);
+
+		if ( yr > 0.008856 )
+			yr =  (float) Math.pow(yr, 1/3.);
+		else
+			yr = (float) ((7.787 * yr) + 16 / 116.0);
+
+		if ( zr > 0.008856 )
+			zr =  (float) Math.pow(zr, 1/3.);
+		else
+			zr = (float) ((7.787 * zr) + 16 / 116.0);
+
+
+		double[] lab = new double[3];
+
+		lab[0] = (116*yr)-16;
+		lab[1] = 500*(xr-yr); 
+		lab[2] = 200*(yr-zr); 
+
+		return lab;
+
+
 	}
 	
 	private double dL_CIE2000(float[] x, float[] y) {
