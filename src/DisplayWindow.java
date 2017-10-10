d1ccf10993c634b5f243f8dc094e26d10ac2c6c7
hagin <hagin@hagin-ThinkPad-T440p>
2017-10-09 23:33:55 -0400

    Massive update completed over a weekend. Fully functional as intended

--- src/DisplayWindow.java
+++ src/DisplayWindow.java
@@ -24,8 +24,6 @@ public class DisplayWindow {
 		}
 	};
 
-
-	
 	private void layDownButtons(JButton [] allButtons, int column , int startingButton, int endingButton, int yLevel){
 		int [] allXCoordinates = {20,80,140,200,260,320,380,440,500,560,620,680};	
 		int [] allYCoordinates = {50,110,170,230,290,350,410,470,530};
